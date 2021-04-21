b6cd4100cbd797ad0d004e22cb8c955e996e0f30
Lance Barger <david.barger@cbre.com>
2021-04-21 08:56:14 -0500

    first tf

--- main.tf
+++ main.tf
@@ -34,3 +34,35 @@ resource "azurerm_storage_container" "iot" {
   storage_account_name  = azurerm_storage_account.iot.name
   container_access_type = "private"
 }
+
+resource "azurerm_iothub" "example" {
+  name                = "Example-IoTHub"
+  resource_group_name = azurerm_resource_group.iot.name
+  location            = azurerm_resource_group.iot.location
+
+  sku {
+    name     = "S1"
+    capacity = "1"
+  }
+
+  endpoint {
+    type                       = "AzureIotHub.StorageContainer"
+    connection_string          = azurerm_storage_account.iot.primary_blob_connection_string
+    name                       = "export"
+    batch_frequency_in_seconds = 60
+    max_chunk_size_in_bytes    = 10485760
+    container_name             = azurerm_storage_container.iot.name
+    encoding                   = "Avro"
+    file_name_format           = "{iothub}/{partition}_{YYYY}_{MM}_{DD}_{HH}_{mm}"
+  }
+
+  route {
+    name           = "export"
+    source         = "DeviceMessages"
+    condition      = "true"
+    endpoint_names = ["export"]
+    enabled        = true
+  }
+
+  
+}
