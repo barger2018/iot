ffc8518e43184c5f9d094eeffa9ca7b983750bc9
Lance Barger <david.barger@cbre.com>
2021-04-21 16:57:25 -0500

    container reg

--- main.tf
+++ main.tf
@@ -72,6 +72,6 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name      = azurerm_resource_group.iot.name
   location                 = azurerm_resource_group.iot.location
   sku                      = "Premium"
-  admin_enabled            = false
+  admin_enabled            = true
   georeplication_locations = ["East US", "West Europe"]
 }
