ca999d069009a8551053a7582dfb8e5a2820ea82
Lance Barger <david.barger@cbre.com>
2021-04-21 08:43:15 -0500

    first tf

--- main.tf
+++ main.tf
@@ -20,3 +20,17 @@ resource "azurerm_resource_group" "iot" {
   name     = "iot"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "iot" {
+  name                     = "iot-storage"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "iot" {
+  name                  = "iot-container"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
