beff82322f1cd484f0f02fef970057cc11ac8311
Lance Barger <david.barger@cbre.com>
2021-04-21 09:05:22 -0500

    first tf

--- main.tf
+++ main.tf
@@ -35,34 +35,34 @@ resource "azurerm_storage_container" "iot" {
   container_access_type = "container"
 }
 
-resource "azurerm_iothub" "example" {
-  name                = "Example-IoTHub"
-  resource_group_name = azurerm_resource_group.iot.name
-  location            = azurerm_resource_group.iot.location
+# resource "azurerm_iothub" "example" {
+#   name                = "Example-IoTHub"
+#   resource_group_name = azurerm_resource_group.iot.name
+#   location            = azurerm_resource_group.iot.location
 
-  sku {
-    name     = "S1"
-    capacity = "1"
-  }
+#   sku {
+#     name     = "S1"
+#     capacity = "1"
+#   }
 
-  endpoint {
-    type                       = "AzureIotHub.StorageContainer"
-    connection_string          = azurerm_storage_account.iot.primary_blob_connection_string
-    name                       = "export"
-    batch_frequency_in_seconds = 60
-    max_chunk_size_in_bytes    = 10485760
-    container_name             = azurerm_storage_container.iot.name
-    encoding                   = "Avro"
-    file_name_format           = "{iothub}/{partition}_{YYYY}_{MM}_{DD}_{HH}_{mm}"
-  }
+#   endpoint {
+#     type                       = "AzureIotHub.StorageContainer"
+#     connection_string          = azurerm_storage_account.iot.primary_blob_connection_string
+#     name                       = "export"
+#     batch_frequency_in_seconds = 60
+#     max_chunk_size_in_bytes    = 10485760
+#     container_name             = azurerm_storage_container.iot.name
+#     encoding                   = "Avro"
+#     file_name_format           = "{iothub}/{partition}_{YYYY}_{MM}_{DD}_{HH}_{mm}"
+#   }
 
-  route {
-    name           = "export"
-    source         = "DeviceMessages"
-    condition      = "true"
-    endpoint_names = ["export"]
-    enabled        = true
-  }
+#   route {
+#     name           = "export"
+#     source         = "DeviceMessages"
+#     condition      = "true"
+#     endpoint_names = ["export"]
+#     enabled        = true
+#   }
 
   
-}
+# }
